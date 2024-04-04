191d74fff83c031af9a9993c5ae0bc3979fc4d4d
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-04-04 13:59:24 -0300

    Modificações no arquivo que cuida das operações do banco para adicionar a funcionalidade de atualização de contatos

--- config/process.php
+++ config/process.php
@@ -37,17 +37,38 @@
         $data = $_POST;
 
         if($data['type'] == 'create'){
-            $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observarions)";
+            $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
 
             $stmt = $conn->prepare($query);
             
             $stmt->bindParam(':name', $data['name']);
             $stmt->bindParam(':phone', $data['phone']);
-            $stmt->bindParam(':observarions', $data['observarions']);
+            $stmt->bindParam(':observations', $data['observations']);
             
             try{
                 $stmt->execute();
                 $_SESSION["msg"] = "Contato adicionado com sucesso!";
+            }
+            catch(PDOException $e){
+                // Erro no cadastro
+                $error = $e->getMessage();
+                echo "Erro: $error <br>";
+            }
+        }
+
+        if($data['type'] == 'edit'){
+            $query = "UPDATE contacts SET name = :name, phone = :phone, observations = :observations WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+            
+            $stmt->bindParam(':name', $data['name']);
+            $stmt->bindParam(':phone', $data['phone']);
+            $stmt->bindParam(':observations', $data['observations']);
+            $stmt->bindParam(':id', $data['id']);
+            
+            try{
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato atualizado com sucesso!";
             }
             catch(PDOException $e){
                 // Erro no cadastro
