b3c8242d6bee1e46d7e85e9b5063cef2df219eea
João Pedro Pazos Ramos <jpedropazosramos@gmail.com>
2024-04-04 13:31:51 -0300

    Modificações no arquivo que cuida das operações do banco para adicionar a funcionalidade de cadastro de contatos

--- config/process.php
+++ config/process.php
@@ -6,26 +6,56 @@
     include_once('connection.php');
     include_once('url.php');
 
-    if(!empty($_GET)){
-        // Retorna contato específico
-        $id = $_GET['id'];
-        
-        $query = "SELECT * FROM contacts WHERE id = :id";
+    if(empty($_POST)){
+        // Nenhum dado foi enviado por POST
+        if(!empty($_GET)){
+            // Retorna contato específico
+            $id = $_GET['id'];
+            
+            $query = "SELECT * FROM contacts WHERE id = :id";
 
-        $stmt = $conn->prepare($query);
-        
-        $stmt->bindParam(':id', $id);
+            $stmt = $conn->prepare($query);
+            
+            $stmt->bindParam(':id', $id);
 
-        $stmt->execute();
+            $stmt->execute();
 
-        $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+            $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        else{
+            // Retorna todos os contatos
+            $query = "SELECT * FROM contacts";
+
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+
+            $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
     else{
-        // Retorna todos os contatos
-        $query = "SELECT * FROM contacts";
+        // Algum dado foi enviado por POST
+        $data = $_POST;
 
-        $stmt = $conn->prepare($query);
-        $stmt->execute();
+        if($data['type'] == 'create'){
+            $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observarions)";
 
-        $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt = $conn->prepare($query);
+            
+            $stmt->bindParam(':name', $data['name']);
+            $stmt->bindParam(':phone', $data['phone']);
+            $stmt->bindParam(':observarions', $data['observarions']);
+            
+            try{
+                $stmt->execute();
+            }
+            catch(PDOException $e){
+                // Erro no cadastro
+                $error = $e->getMessage();
+                echo "Erro: $error <br>";
+            }
+        }
+
+        header('Location:' . $BASE_URL . '../index.php');
     }
+
+    $conn = null;
